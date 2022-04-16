e5b550a6335740042a8d223cee35865a14b95a9f
Amos Oneh <89107139+Amosoneh@users.noreply.github.com>
2022-04-16 12:51:50 +0100

    Add files via upload

---
+++ InterestCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class InterestCalculator{
+	public static void main(String... args){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter balance and interest rate: ");
+		int balance = input.nextInt();
+		double interestRate = input.nextDouble();
+
+		double interest = balance * (interestRate/1200);
+
+		System.out.printf("The interest is %f%n",interest);		
+		
+		
+	}
+}
