81102681813090fa46c217b3ae4f16620e70a2cf
Amosoneh <amoskhaled@gmail.com>
2022-05-05 11:23:53 +0100

    Exercise 2.18

---
+++ src/chapter2Assignment/DisplayingShape.java
@@ -0,0 +1,53 @@
+package chapter2Assignment;
+
+public class DisplayingShape {
+    public static void main(String[] args) {
+
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+
+
+
+        System.out.println("   ***");
+        System.out.println(" *     *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println(" *     *");
+        System.out.println("   ***");
+
+
+
+        System.out.println("  * ");
+        System.out.println(" *** ");
+        System.out.println("***** ");
+        System.out.println("  * ");
+        System.out.println("  * ");
+        System.out.println("  * ");
+        System.out.println("  * ");
+        System.out.println("  * ");
+        System.out.println("  * ");
+
+
+
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *     * ");
+        System.out.println("*       *");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    * ");
+    }
+
+}
