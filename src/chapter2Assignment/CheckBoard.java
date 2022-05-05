889a901b47807bb36324129bc07fc3b99fd8113d
Amosoneh <amoskhaled@gmail.com>
2022-05-05 11:25:41 +0100

    Exercise 2.27

---
+++ src/chapter2Assignment/CheckBoard.java
@@ -0,0 +1,15 @@
+package chapter2Assignment;
+
+public class CheckBoard {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+        System.out.println("* * * * * * * *");
+        System.out.println(" * * * * * * * *");
+
+    }
+}
