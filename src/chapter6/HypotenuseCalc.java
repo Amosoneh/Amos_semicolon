ac4d66abab5410848c7502a43e3b300aed852a7b
Amosoneh <amoskhaled@gmail.com>
2022-05-17 12:48:21 +0100

    Hypotenuse of a right angle triangle calculator

---
+++ src/chapter6/HypotenuseCalc.java
@@ -0,0 +1,14 @@
+package chapter6;
+
+public class HypotenuseCalc {
+    public static void main(String[] args) {
+        System.out.println(hypotenuse(3,3));
+    }
+
+    private static double hypotenuse(double num1, double num2){
+        double a = num1 * num1;
+        double b = num2 * num2;
+        double c = a + b;
+        return Math.sqrt(c);
+    }
+}
